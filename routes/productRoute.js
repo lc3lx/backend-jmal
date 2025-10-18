c263e16b722e0760ced8687b57f75f167bfab5e6
lc3lx <lc3lx@users.noreply.github.com>
2025-10-18 07:49:30 +0300

    fish

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const {
   getProductValidator,
   createProductValidator,
   updateProductValidator,
   deleteProductValidator,
-} = require('../utils/validators/productValidator');
+} = require("../utils/validators/productValidator");
 
 const {
   getProducts,
@@ -14,34 +14,28 @@ const {
   deleteProduct,
   uploadProductImages,
   resizeProductImages,
-} = require('../services/productService');
-const authService = require('../services/authService');
-const reviewsRoute = require('./reviewRoute');
+} = require("../services/productService");
+const authService = require("../services/authService");
 
 const router = express.Router();
 
-// POST   /products/jkshjhsdjh2332n/reviews
-// GET    /products/jkshjhsdjh2332n/reviews
-// GET    /products/jkshjhsdjh2332n/reviews/87487sfww3
-router.use('/:productId/reviews', reviewsRoute);
-
 router
-  .route('/')
+  .route("/")
   .get(getProducts)
   .post(
     authService.protect,
-    authService.allowedTo('admin', 'manager'),
+    authService.allowedTo("admin", "manager"),
     uploadProductImages,
     resizeProductImages,
     createProductValidator,
     createProduct
   );
 router
-  .route('/:id')
+  .route("/:id")
   .get(getProductValidator, getProduct)
   .put(
     authService.protect,
-    authService.allowedTo('admin', 'manager'),
+    authService.allowedTo("admin", "manager"),
     uploadProductImages,
     resizeProductImages,
     updateProductValidator,
@@ -49,7 +43,7 @@ router
   )
   .delete(
     authService.protect,
-    authService.allowedTo('admin'),
+    authService.allowedTo("admin"),
     deleteProductValidator,
     deleteProduct
   );
